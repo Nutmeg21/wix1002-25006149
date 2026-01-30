d3f882137e053b1c668a23f8e0b75ca65582245c
Loo Jiun Wei <25006149@siswa.um.edu.my>
2026-01-30 17:04:27 +0800

    2024_2 q1

---
+++ pastyear/2024_2/Q1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[][] grid = new String[3][3];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                grid[i][j] = sc.next();
+            }
+        }
+        
+    }
+}
